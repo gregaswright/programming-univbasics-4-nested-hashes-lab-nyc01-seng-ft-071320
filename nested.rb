aed720827e1ba674ca03d24f915e8ac9c7573f37
Gregory A.S. Wright <geslim@gmail.com>
2020-06-28 13:49:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,10 +65,7 @@ def adding_matz
 #   :languages => ["LISP", "C"]
 # }
 # return the entire updated hash
-programmer_hash[:yukihiro_matsumoto] = [
- { known_for: "Ruby" },
- { languages: ["LISP", "C"]}
-]
+
 	programmer_hash =
  		{
       :grace_hopper => {
@@ -84,6 +81,10 @@ programmer_hash[:yukihiro_matsumoto] = [
         :languages => ["C"]
       }
     }
+		programmer_hash[:yukihiro_matsumoto] = [
+	 	{ known_for: "Ruby" },
+	 	{ languages: ["LISP", "C"]}
+	 ]
 end
 
 def changing_alan
